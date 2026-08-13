00e3cb9dd28ad01bdfb5cb5fd628cbf02f0e3945
sharafa99 <abdulrazaqsharafa@gmail.com>
2026-08-13 11:03:42 +0100

    Program modified to accept argument through the terminal

--- Python Basic Exercise/Arithmetic Product and Conditional Logic/main.py
+++ Python Basic Exercise/Arithmetic Product and Conditional Logic/main.py
@@ -1,7 +1,9 @@
+val1 = int(input("What is the first number: "))
+val2 = int(input("What is the second number: "))
 def calc(num1, num2):
     if num1 * num2 <= 1000:
         return (num1 * num2)
     else:
         return (num1 + num2)
 
-print(calc(40, 30))
+print(calc(val1, val2))
