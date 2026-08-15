d086a38700c3e9b4e9caae31dfa8629d277b8379
sharafa99 <abdulrazaqsharafa@gmail.com>
2026-08-15 15:02:43 +0100

    Exercise completed

---
+++ Python list Exercises/Exercise1.py
@@ -0,0 +1,8 @@
+# numbers = [10, 20, 30, 40, 50]
+numbers = list((10, 20, 30, 40, 50))
+
+list_content = len(numbers) == 0
+
+print(f"Third element: {numbers[2]}")
+print(f"Length of list: {len(numbers)}")
+print(f"Is the list empty: {list_content}")
